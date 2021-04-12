bde1d79139aae3f0e2596009fcbcb9daf95ba8f5
duongtv <duongtv2712@gmail.com>
2021-04-12 03:17:30 -0700

    create migrate

---
+++ database/migrations/2021_04_12_095449_create_friends_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('from_id');
+            $table->foreign('from_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('to_id');
+            $table->foreign('to_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
