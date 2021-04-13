2c4136c4b986be4524292bd29432b3452c38fb32
duongtv <duongtv2712@gmail.com>
2021-04-12 23:42:55 -0700

    auth jwt& reset password

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/refresh', 'AuthController@refresh');
+    Route::get('/user-profile', 'AuthController@userProfile');
+    Route::post('/reset-password-request', 'PasswordResetRequestController@sendPasswordResetEmail');
+    Route::get('/check-token-reset-password-at/{token}', 'PasswordResetRequestController@checkTokenResetPasswordAt');
+    Route::post('/change-password', 'PasswordResetRequestController@changePassword');
+});
